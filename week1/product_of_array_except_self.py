8f06559ed6ee96fd6a8dfee4064304452c45a64b
Eraliev006 <eraliev.adilet006@gmail.com>
2026-01-05 01:05:59 +0600

    add comments and description solve

--- week1/product_of_array_except_self.py
+++ week1/product_of_array_except_self.py
@@ -16,17 +16,18 @@ Output: [0,0,9,0,0]
 
 def productExceptSelf(nums: List[int]) -> List[int]:
     n = len(nums)
-    res = [1] * n
+    res = [1] * n # 1
 
-    prefix = 1
-    for i in range(n):
-        res[i] = prefix
-        prefix *= nums[i]    
-    postfix = 1
-    for i in range(n - 1, -1, -1):
-        res[i] *= postfix
-        postfix *= nums[i]  
-    return res
+    prefix = 1 # 2.1
+    for i in range(n): # 2.2, 
+        res[i] = prefix # 2.3
+        prefix *= nums[i] # 2.4
+
+    postfix = 1 # 3.1
+    for i in range(n - 1, -1, -1): # 3.2
+        res[i] *= postfix # 3.3
+        postfix *= nums[i]  # 3.4
+    return res # 3.5
 
 
 """ 
@@ -36,8 +37,44 @@ input: [1,2,3,4]
 Output: [24,12,8,6]
 
 
+1. create a list res=[1,1,1,1]
 
+2.1 collect a prefix for left side
 
+2.2 i = 0, prefix = 1, res[i] = 1, nums[i] = 1
+2.3 save for — res[i] = prefix — [1,...]
+2.4 prefix * nums[i] — prefix = 1
 
+2.2 i = 1, prefix = 1, res[i] = 1, nums[i] = 2
+2.3 save for — res[i] = prefix — [1,1...]
+2.4 prefix * nums[i] — prefix = 2
+
+2.2 i = 2, prefix = 2, res[i] = 1, nums[i] = 3
+2.3 save for — res[i] = prefix — [1,1,2...]
+2.4 prefix * nums[i] — prefix = 6
+
+2.2 i = 3, prefix = 6, res[i] = 1, nums[i] = 4
+2.3 save for — res[i] = prefix — [1,1,2,6]
+2.4 prefix * nums[i] — prefix = 24
+
+# RES = [1,1,2,6]
+
+3.1 collect postfix from right side
+
+3.2 i = 3, postfix = 1, res[i] = 6, nums[i] = 4
+3.3 save for — res[i] *= postfix — [..., 6]
+3.4 postfix *= nums[i] = 4
+
+3.2 i = 2, postfix = 4, res[i] = 2, nums[i] = 3
+3.3 save for — res[i] *= postfix — [.., 8,6]
+3.4 postfix *= nums[i] = 12
+
+3.2 i = 1, postfix = 12, res[i] = 1, nums[i] = 2
+3.3 save for — res[i] *= postfix — [.., 12,8,6]
+3.4 postfix *= nums[i] = 24
+
+3.2 i = 0, postfix = 24, res[i] = 1, nums[i] = 1
+3.3 save for — res[i] *= postfix — [24,12,8,6]
+3.4 postfix *= nums[i] = 24
 
 """
